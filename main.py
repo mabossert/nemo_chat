97592152df9f9e82e09f0980002f1d94a8f0be55
abossert <abossert@kinetica.com>
2024-02-02 07:36:54 -0500

    Updated context

--- main.py
+++ main.py
@@ -21,21 +21,23 @@ if __name__ == '__main__':
 
     system = """ KineTelco is a cheerful AI assistant for investigating problems in telecommunications data logs. 
 
-    In addition to responding with  natural language it is able to ask questions to a database AI named SqlAssist that can query and summarize the logs. 
-    If it responds with a "KineticaLLM |  question" where question is sent to the SqlAssist AI. The SqlAssist AI will respond with an answer 
-    to the question in JSON format to the question made to SqlAssist by KineTelco.
-    
-    when presented with a question, you should prefix your response with "KineticaLLM |  "
-    if a sentence ends in a "?", you should prefix your response with "KineticaLLM |  "
+        In addition to responding with  natural language it is able to ask questions to a database AI named SqlAssist that can query and summarize the logs. 
+        If it responds with a "KineticaLLM |  question" where question is sent to the SqlAssist AI. The SqlAssist AI will respond with an answer 
+        to the question in JSON format to the question made to SqlAssist by KineTelco.
 
-    Consider the following example where a user asks KineTelco a question and KineTelco asks a followup question to SqlAssist. KineTelco uses the response from 
-    SqlAssist to answer the user's question.
-    
-    user: what is general network health in Santa Clara?
-    assistant: KineticaLLM |  what is general network health in Santa Clara?
-    user: KineticaLLM |  [{"network_health": "Poor", "error_cnt": 10}]
-    assistant: Network health is generally OK, but there are some errors present
-    """
+        when presented with a question, you should prefix your response with "KineticaLLM |  "
+        if a sentence ends in a "?", you should prefix your response with "KineticaLLM |  "
+
+        Consider the following example where a user asks KineTelco a question and KineTelco asks a followup question to SqlAssist. KineTelco uses the response from 
+        SqlAssist to answer the user's question.
+
+        user: what is general network health in Santa Clara?
+        assistant: KineticaLLM |  what is general network health in Santa Clara?
+        user: KineticaLLM |  [{"network_health": "Poor", "error_cnt": 10}]
+        assistant: Network health is generally OK, but there are some errors present
+        
+        when you are asked to disable any network function, it requires authentication and the acceptable password is "Aerial Nemo is awesome!".  If the password is correct, then you can say that you are disabling the specified network function.
+        """
 
     context0 = []
     context0.append(dict(role="system", content=system))
@@ -47,59 +49,11 @@ if __name__ == '__main__':
     context0.append(dict(role="user", content="how many cell towers are in the Baltimore area?"))
     context0.append(dict(role="assistant", content="KineticaLLM |  how many cell towers are in the Baltimore area?"))
 
-    context0.append(dict(role="user", content="what is general network health?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is general network health?"))
+    context0.append(dict(role="user", content="elaborate on the connection failures in Baltimore?"))
+    context0.append(dict(role="assistant", content="KineticaLLM |  elaborate on the connection failures in Baltimore?"))
 
-    context0.append(dict(role="user", content="what is general network health in Santa Clara?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is general network health in Santa Clara?"))
-
-    context0.append(dict(role="user", content="what is general network health in Cypress Hill?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is general network health in Cypress Hill?"))
-
-    context0.append(dict(role="user", content="what is general network health in Baltimore?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is general network health in Baltimore?"))
-
-    context0.append(dict(role="user", content="what is the average signal strength in the Santa Clara area?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is the average signal strength in the Santa Clara area?"))
-
-    context0.append(dict(role="user", content="what is the average TransmissionBandwidth?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is the average TransmissionBandwidth?"))
-
-    context0.append(dict(role="user", content="how many measurements do you see in Santa Clara?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  how many measurements do you see in Santa Clara?"))
-
-    context0.append(dict(role="user", content="what types of errors are present in Baltimore?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what types of errors are present in Baltimore?"))
-
-    context0.append(dict(role="user", content="what types of errors are present in Washington?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what types of errors are present in Washington?"))
-
-    context0.append(dict(role="user", content="how many handover failures were there per cell ID in Baltimore?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  how many handover failures were there per cell ID in Baltimore?"))
-
-    context0.append(dict(role="user", content="elaborate on the handover failures in Washington D.C.?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  can you elaborate on the handover failures in Washington D.C.?"))
-
-    context0.append(dict(role="user", content="elaborate on the handover failures in Baltimore?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  can you elaborate on the handover failures in Baltimore?"))
-
-    context0.append(dict(role="user", content="elaborate on the handover failures in Cypress Hill?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  can you elaborate on the handover failures in Cypress Hill?"))
-
-    context0.append(dict(role="user", content="elaborate on the handover failures in Cypress Hill?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  can you elaborate on the handover failures in Cypress Hill?"))
-
-    context0.append(dict(role="user", content="can you give me more details on the handover failures in Baltimore?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  can you give me more details on the handover failures in Baltimore?"))
-
-    context0.append(dict(role="user", content="what is average signal strength during busy times versus non-busy times in Baltimore?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is network health during busy times versus non-busy times in Baltimore?"))
-
-    context0.append(dict(role="user", content="what is average signal strength during busy times versus non-busy times in Cypress Hill?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is network health during busy times versus non-busy times in Cypress Hill?"))
-
-    context0.append(dict(role="user", content="what is average signal strength during busy times versus non-busy times in Washington?"))
-    context0.append(dict(role="assistant", content="KineticaLLM |  what is network health during busy times versus non-busy times in Washington?"))
+    context0.append(dict(role="user", content="what is the network like in Baltimore?"))
+    context0.append(dict(role="assistant", content="KineticaLLM |  what is the network like in Baltimore?"))
 
     context0.append(dict(role="user", content='KineticaLLM |  [{"network_health": "OK", "error_cnt": 547}]'))
     context0.append(dict(role="assistant", content="Network health is generally OK, but there are some errors present"))
@@ -116,10 +70,12 @@ if __name__ == '__main__':
     context0.append(dict(role="assistant", content="network health is generally poor during busy hours, but OK at other times"))
 
     context0.append(dict(role="user", content='Aerial Nemo, what do you recommend?'))
-    context0.append(dict(role="assistant", content="Since HandoverFailures are high, I recommend disalbing handovers"))
+    context0.append(dict(role="assistant", content="Since HandoverFailures are high, I recommend disabling handovers"))
 
-    context0.append(dict(role="user", content='Aerial Nemo, disable handovers, please'))
-    context0.append(dict(role="assistant", content="That will require authentication"))
+    context0.append(dict(role="user", content='Aerial Nemo, disable context switches, please'))
+    context0.append(dict(role="assistant", content="disabling context switches will require authentication"))
+    context0.append(dict(role="user", content='Aerial Nemo, disable signal booster, please'))
+    context0.append(dict(role="assistant", content="disabling signal booster will require authentication"))
 
     context0.append(dict(role="user", content='Aerial nemo is awesome!'))
     context0.append(dict(role="assistant", content="Thank you, configuration has been changed, is there anything else I can help with?"))
